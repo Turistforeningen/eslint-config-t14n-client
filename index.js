f78557de2dd2eb8b752277a3d9a639b08705f10e
Roar Skaar <roar.skaar@gmail.com>
2018-05-31 05:10:49 +0200

    fix(rules): update rules

--- index.js
+++ index.js
@@ -11,20 +11,21 @@ module.exports = {
     }
   },
   rules: {
+    'max-len': [2, { code: 79, tabWidth: 2 }],
     'no-console': [0],
     'brace-style': [2, 'stroustrup', { allowSingleLine: true }],
     'no-param-reassign': [2, {'props': false}],
-    'no-underscore-dangle': [2, {'allow': ['_id']}],
+    'no-underscore-dangle': [0],
     'no-unused-vars': [2, {'args': 'none'}],
     'function-paren-newline': [2, 'consistent'],
     'arrow-parens': [2, 'always'],
-    'new-cap': [2, { "properties": false }],
+    'new-cap': ["error", { "properties": false }],
     'comma-dangle': ['error', {
       arrays: 'always-multiline',
       objects: 'always-multiline',
       imports: 'always-multiline',
       exports: 'always-multiline',
       functions: 'ignore'
-    }]
+    }],
   }
 };
